957b03942f11e6d383bbefb86a8ef39c34c4ded6
kwaimuk <teaelixer@gmail.com>
2017-05-01 18:23:44 -1000

    completed connected routes for index to redirect to gallery

--- routes/galleryRoutes.js
+++ routes/galleryRoutes.js
@@ -3,10 +3,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+router.route('/')
+      .post((req, res) => {
+        res.render('new', null);
+      })
+      // Retrieves the index page
+      .get((req, res) => {
+        console.log(db.Picture.findAll());
+        res.render('index', null);
+      });
+
 router.route('/new')
       // Retrieves the index page
       .get((req, res) => {
         res.render('new', null);
       });
+
+
 
 module.exports = router;
