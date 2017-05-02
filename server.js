96f506591bd272369b16651846b92f07e2c0b7ea
Dakota <bazookaharwell@gmail.com>
2017-05-01 16:20:37 -1000

    Intialized server and Sequelize

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const PORT = process.env.port || 3000;
+
+// Routes
+app.use('/', indexRoutes);
+
+// Initialize server
+const server = app.listen(PORT, () => {
+  db.sequelize.sync();
+});
+
+// For testing
+module.exports = server;
