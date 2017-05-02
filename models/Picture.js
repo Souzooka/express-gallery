73f7e90cde98ddf5d7f440a25d0b4a4e298a789e
Dakota <bazookaharwell@gmail.com>
2017-05-01 16:37:07 -1000

    Debug Picture model

--- models/Picture.js
+++ models/Picture.js
@@ -2,13 +2,14 @@ module.exports = function(sequelize, DataTypes) {
   var Picture = sequelize.define("Picture", {
     author: DataTypes.STRING,
     link: DataTypes.STRING,
-    description: DataTypes.TEXT,
+    description: DataTypes.STRING
+  }, {
     classMethods: {
       associate: function(models) {
-        Task.belongsTo(models.User);
+        Picture.belongsTo(models.User);
       }
     }
   });
 
-  return Task;
+  return Picture;
 };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const db = require('./models');
 const handlebars = require('express-handlebars');
 const PORT = process.env.port || 3000;
 
 // Routes
-app.use('/', indexRoutes);
+// app.use('/', indexRoutes);
 
 // Initialize server
 const server = app.listen(PORT, () => {
