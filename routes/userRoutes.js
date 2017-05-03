e14729314edcf75cf8d72809375b7a0e4647d299
kwaimuk <teaelixer@gmail.com>
2017-05-03 11:21:07 -1000

    Created POST method for login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,28 @@ router.route('/create')
       // Retrieves the index page
       .get((req, res) => {
         res.render('newUser', null);
+      })
+
+      .post((req, res) => {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+          bcrypt.hash(req.body.password, salt, function(err, hash) {
+            User.create({
+              username: req.body.username,
+              password: hash
+            })
+            .then( (user) => {
+              console.log(user);
+              res.redirect('/login');
+            });
+          });
+        });
       });
+
+router.route('/login')
+      // Retrieves the index page
+      .get((req, res) => {
+        res.render('login', null);
+      });
+
 
 module.exports = router;
